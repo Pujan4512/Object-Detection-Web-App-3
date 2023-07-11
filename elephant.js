0ce16906f9400f8b1a37a9353f782bc925003b78
Pujan4512 <110390331+Pujan4512@users.noreply.github.com>
2023-07-11 21:41:27 +0530

    Add files via upload

---
+++ elephant.js
@@ -0,0 +1,42 @@
+objects = [];
+status = "";
+
+function preload(){
+    img = loadImg("Img_3.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    status = true;
+    object_Detector.detect(img, gotResults);
+}
+
+function gotResults(results){
+    console.log(results);
+
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 380, 380);
+
+    if(status != ""){
+        for(i=0;i<objects.length;i++){
+            percent = floor(objects[i].confidence * 100 + "%");
+            console.log(percent);
+            text(objects[i].label + " " + percent, objects[i].x+15, objects[i].y+15);
+            
+            stroke("#FF0000");
+            fill("#FF0000");
+            noFill();
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
